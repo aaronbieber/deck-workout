b75e1b368f9ecdacc37e3ffc33f1738a7d391dc4
Aaron Bieber <aaron@aaronbieber.com>
2018-05-18 06:13:08 -0400

    Move reducer branches into own functions
    This helps keep variable scoping sane

--- src/reducers/workout.js
+++ src/reducers/workout.js
@@ -33,60 +33,80 @@ const spliceExercise = exercises => {
     return exercises.splice(Math.floor(Math.random() * exercises.length), 1)[0];
 }
 
+const cloneState = (state) => {
+    return JSON.parse(JSON.stringify(state));
+}
+
+const generate = (state) => {
+    var groups = Object.keys(data);
+    var randGroup = groups[Math.floor(Math.random() * groups.length)];
+    var localData = {};
+
+    // slice(0) is a trick to get a clone of an array; we have to
+    // create a "deep clone" of this object because splice() is
+    // destructive (on purpose, to prevent duplicates)
+    groups.forEach(group => localData[group] = data[group].slice(0));
+
+    var newExercises = {
+        'hearts':   spliceExercise(localData["upper"])["name"],
+        'diamonds': spliceExercise(localData["lower"])["name"],
+        'clubs':    spliceExercise(localData["core"])["name"],
+        'spades':   spliceExercise(localData[randGroup])["name"]
+    };
+
+    console.log('generate: draw count: ' + state.drawCount);
+    return Object.assign({}, state, {
+        exercises: newExercises,
+        deck: buildDeck(),
+        draw: []
+    });
+}
+
+const draw = (state, drawCountPref) => {
+    var newState = cloneState(state);
+    var drawCount = Math.min(newState["deck"].length, state.drawCount);
+
+    console.log(newState);
+
+    for (var i=0; i<drawCount; i++) {
+        newState["draw"].push(
+            newState["deck"].splice(Math.floor(Math.random() * newState["deck"].length), 1)
+        );
+    }
+    console.log(newState);
+    return newState;
+}
+
+const drawThree = (state) => {
+    console.log('toggle draw three');
+    console.log('draw count: ' + state.drawCount);
+    var drawCount;
+
+    if (state.drawCount === 3) {
+        drawCount = 1;
+    } else {
+        drawCount = 3;
+    }
+
+    var newState = Object.assign({}, state, {
+        drawCount
+    });
+
+    console.log(newState);
+
+    return newState;
+}
+
 export default function workout(state = initialState, action) {
     switch (action.type) {
     case types.GENERATE:
-        var groups = Object.keys(data);
-        var randGroup = groups[Math.floor(Math.random() * groups.length)];
-        var localData = {};
-
-        // slice(0) is a trick to get a clone of an array; we have to
-        // create a "deep clone" of this object because splice() is
-        // destructive (on purpose, to prevent duplicates)
-        groups.forEach(group => localData[group] = data[group].slice(0));
-
-        var newExercises = {
-            'hearts':   spliceExercise(localData["upper"])["name"],
-            'diamonds': spliceExercise(localData["lower"])["name"],
-            'clubs':    spliceExercise(localData["core"])["name"],
-            'spades':   spliceExercise(localData[randGroup])["name"]
-        };
-        console.log('generate: draw count: ' + state.drawCount);
-        return Object.assign({}, state, {
-            exercises: newExercises,
-            deck: buildDeck()
-        });
+        return generate(state);
 
     case types.DRAW:
-        var newState = JSON.parse(JSON.stringify(state));
-        console.log(newState);
-
-        for (var i=0; i<state.drawCount; i++) {
-            newState["draw"].push(
-                newState["deck"].splice(Math.floor(Math.random() * newState["deck"].length), 1)
-            );
-        }
-        console.log(newState);
-        return newState;
+        return draw(state, state.drawCount);
 
     case types.TOGGLE_DRAW_THREE:
-        console.log('toggle draw three');
-        console.log('draw count: ' + state.drawCount);
-        var drawCount;
-
-        if (state.drawCount === 3) {
-            drawCount = 1;
-        } else {
-            drawCount = 3;
-        }
-
-        newState = Object.assign({}, state, {
-            drawCount
-        });
-
-        console.log(newState);
-
-        return newState;
+        return drawThree(state);
 
     default:
     return state;
