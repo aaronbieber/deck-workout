2a36c03346cfc78c7c5f341ba9e6acdc4b1a675b
Aaron Bieber <aaron@aaronbieber.com>
2018-06-04 08:43:52 -0400

    Remove unnecessary semicolons

--- src/reducers/workout.js
+++ src/reducers/workout.js
@@ -1,5 +1,5 @@
-import * as types from '../actions/actionTypes';
-import data from '../data/exercises';
+import * as types from '../actions/actionTypes'
+import data from '../data/exercises'
 import { cloneObject } from '../utils'
 
 const initialState = {
