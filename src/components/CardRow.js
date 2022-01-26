f91dc2ed130d59aef6a1be2833e67d2107c3b177
Aaron Bieber <aaron@aaronbieber.com>
2022-01-26 07:28:57 -0500

    Indicate which suit is being edited

--- src/components/CardRow.js
+++ src/components/CardRow.js
@@ -29,13 +29,14 @@ export default class CardRow extends Component {
         var suitSymbol = suitSymbols[this.props.suit]["symbol"];
         var suitColor  = suitSymbols[this.props.suit]["color"];
         var suitClass  = "suit-" + suitColor;
+        var classes = "material-icons" + (this.props.suit === this.props.customizingSuit ? " edit" : "");
 
         return (
             <div className="columns">
               <div className="playing-card column col-4 col-md-12 col-mx-auto">
                 { this.props.edit
-                    ? <i className="material-icons" onClick={ this._edit }>edit</i>
-                    : null
+                  ? <i className={ classes } onClick={ this._edit }>edit</i>
+                  : null
                 }
                 <span className={ suitClass }>{ suitSymbol }</span> { this.props.exercise }
               </div>
